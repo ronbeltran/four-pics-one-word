941f4efe9c73e3154cd7d63fded2f87ed4027418
Ronnie Beltran <rbbeltran.09@gmail.com>
2015-07-20 21:35:38 +0800

    Setup jinja2 templates render

--- game/views.py
+++ game/views.py
@@ -1,6 +1,16 @@
+import logging
+from flask import render_template, request
 from game import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return '4 Pics 1 Word Solver'
+    context = {}
+    if request.method == "POST":
+        length = request.form['length']
+        letters = request.form['letters']
+        context.update({
+            'length': int(length),
+            'letters': letters,
+        })
+    return render_template('index.html', **context)
