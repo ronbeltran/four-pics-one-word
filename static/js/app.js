737e9e86624529c57923adca61b5b9b2dc05315c
Ronnie Beltran <rbbeltran.09@gmail.com>
2015-07-21 10:51:20 +0800

    Display possible matches as listview

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,25 @@
+// var gapi = null;
+
 function init() {
     var api_root = '//' + window.location.host + '/_ah/api';
     console.log(api_root);
 
     gapi.client.load('wordsapi', 'v1', function() {
         console.log('wordsapi is loaded');
+//        get_words_api(4, 'yslmwklgnsig');
     }, api_root);
 }
+
+function get_words_api(length, letters){
+    gapi.client.wordsapi.words.get({'length': length, 'choices': letters}).execute(function(resp){
+        console.log(resp);
+    });
+}
+
+$(document).ready(function(){
+    $('#submit').on('click', function(e){
+//        e.preventDefault();
+        console.log('call the endpoint api');
+    });
+});
+
